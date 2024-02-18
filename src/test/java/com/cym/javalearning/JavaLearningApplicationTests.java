d662be7823fd0d908c3c801154d022e10f21003c
tianming_macbookpro <thomastsai365@gmail.com>
2024-02-18 21:20:12 +0800

    fi

---
+++ src/test/java/com/cym/javalearning/JavaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cym.javalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
